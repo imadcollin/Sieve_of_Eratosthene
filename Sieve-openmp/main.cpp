09e3ed2a8915cb4772ac10d015d0affaea6b5760
Imad Collin <Admin@MacBook-Pro.local>
2017-10-11 16:14:21 +0200

    add new target

---
+++ Sieve-openmp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sieve-openmp
+//
+//  Created by Imad Collin on 2017-10-11.
+//  Copyright © 2017 Imad Collin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
